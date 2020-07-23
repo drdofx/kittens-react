9cd7dd8489c983d5c020718b8aecd6710966dde1
drdofx <daffa.arviano@outlook.com>
2020-07-23 09:59:07 +0700

    Updates

---
+++ precache-manifest.fc24efc5a43b006fd156e1a911a5eee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac5256f0752a39f450282502d142d25",
+    "url": "/kittens-react/index.html"
+  },
+  {
+    "revision": "6a288cb536c0df832d70",
+    "url": "/kittens-react/static/css/main.dd4e20f8.chunk.css"
+  },
+  {
+    "revision": "d78657e1f5ebcc187dce",
+    "url": "/kittens-react/static/js/2.1a3440fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittens-react/static/js/2.1a3440fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a288cb536c0df832d70",
+    "url": "/kittens-react/static/js/main.3e894333.chunk.js"
+  },
+  {
+    "revision": "21f80ba3e485929b710e",
+    "url": "/kittens-react/static/js/runtime-main.4de6d78e.js"
+  }
+]);
